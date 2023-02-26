2d30fb7e49aeb59223cd2751a52acec3386aed07
netpes <arbivnetanel@gmail.com>
2023-02-26 19:47:58 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express  = require('express')
+const app = express()
+const Puppeteerfunc = require('./puppeteer')
+
+
+Puppeteerfunc()
+
+
+
+app.listen(8080)
+
+
+/*
+commands:
+    npm i puppeteer express nodemon
+
+
+ */
